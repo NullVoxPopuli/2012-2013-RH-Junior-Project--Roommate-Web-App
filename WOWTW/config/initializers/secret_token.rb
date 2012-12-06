8ac0b3e13bcce4b3353cd58397e5120cdf5b7131
Bill D'Attilio <dattilwa@rose-hulman.edu>
2012-12-05 21:29:49 -0500

    Initial Commit

---
+++ WOWTW/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WOWTW::Application.config.secret_token = '1dcad26862ab7ca122c8e8b97b8d45e23d4012a3cd81e2e999e7d27d2ea003791ed76a1f512915f27301a8227a68623ead93ef9f78df246ed32b06000b4e2a41'
